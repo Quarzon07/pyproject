df23df48dd81427d792eab75391e0e4828be353e
RangerRhino23 <ryadahlke@outlook.com>
2022-11-24 10:36:37 -0600

    new file:   game/assets/cards/temp_card.png 	modified:   game/cardclassTemplate.py

--- game/cardclassTemplate.py
+++ game/cardclassTemplate.py
@@ -1,18 +1,19 @@
 from ursina import *
 
 class card(Entity):
-    def __init__(self):
+    def __init__(self, card_texture):
         super().__init__(
         model = "quad",
         scale = (2.5, 3.5, 1),
         collider = "box",
         color = rgb(255,255,255),
-        position = (0, 0, 0)
+        position = (0, 0, 0),
+        texture = card_texture
         )
         
 app = Ursina()
 
-card1 = card()
+card1 = card('assets/cards/temp_card.png')
 
 app.run()
         
